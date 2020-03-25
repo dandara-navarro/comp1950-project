c643b55be76ab907e341481b3a2d32d2e8120867
dandaradantas <dandaranavarro@gmail.com>
2020-03-25 15:19:33 -0700

    Adding new project structure and inserting footer with php

---
+++ sublinks/footer.php
@@ -0,0 +1,35 @@
+<footer>
+		<section>
+			<h4>About COMP 1950</h4>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+				incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+				exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+				irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+				nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+				officia deserunt mollit anim id est laborum.
+			</p>
+		</section>
+		<section class="right-map">
+			<div class-"site-map">
+				<h4>Site Map</h4>
+				<nav>
+					<ul>
+						<li><a href="#">Lecture</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Resource</a></li>
+						<li><a href="#">BCIT</a></li>
+					</ul>
+				</nav>
+			</div>
+			<div class="map-contact">
+				<h4>Contact</h4>
+				<ul>
+					<li>Jeffery Parker</li>
+					<li><a href="mailto: jeffrey_parker@bcit.ca">jeffrey_parker@bcit.ca</a></li>
+					<li>Office Hours:</li>
+					<li><a href="learn.bcit.ca">Learning Hub</a></li>
+				</ul>
+			</div>
+		</section>
+	</footer>    
